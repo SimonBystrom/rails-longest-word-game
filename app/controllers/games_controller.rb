25b664fd8f6baa95187efb484984d2ab3c5eefc5
Simon Bystrom <simon.bystrom@hyperisland.se>
2021-05-17 15:53:00 +0900

    Finished the logit

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,6 @@
+require 'json'
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
     @letters = []
@@ -6,6 +9,34 @@ class GamesController < ApplicationController
   end
 
   def score
-    raise
+    # write logic for game
+    # 1 get the  current letters from the letters arr
+    @current_letters = params[:current_letters].split(' ')
+    @word = params[:word].split('')
+
+    url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
+    @is_word = JSON.parse(URI.open(url).read)["found"]
+
+
+
+    if @is_word
+      @check = @word.all? do |letter|
+        @current_letters.include?(letter)
+        @current_letters.count(letter) >= @word.count(letter)
+      end
+      @check ? @msg = "#{params[:word]} was a great guess!" : @msg = "Not in the grid"
+    elsif !@is_word
+      @msg = "Not a word"
+    end
+
+      # check ? @msg = "#{params[:word]} was a great guess!" : @msg = "#{params[:word]} is not included in the letters!"
+
+    #   elsif is_word?
+    #       params[:word]
+    #   else
+    #     "not a word"
+    #   end
+    # }
+    p @msg
   end
 end
